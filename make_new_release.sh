d4c54ec274d618bf8be30c659826423bdfcb1ebf
Guillaume Hain <zedtux@zedroot.org>
2016-01-22 20:28:54 +0100

    Uncomment useful code

--- make_new_release.sh
+++ make_new_release.sh
@@ -3,11 +3,11 @@
 DOCKER_IMAGE_NAME="$USER/perfect-scrollbar-rails"
 LIBRARY_NEW_VERSION=`cat lib/**/*.rb | grep VERSION | awk '{ print $3 }' | tr -d "'"`
 
-# LIBRARY_UPDATED=`git status --porcelain | grep -v "lib/perfect-scrollbar-rails/version.rb"`
-# if [[ -n "$LIBRARY_UPDATED" ]]; then
-#   echo "Your repository is not clean !"
-#   exit 1
-# fi
+LIBRARY_UPDATED=`git status --porcelain | grep -v "lib/perfect-scrollbar-rails/version.rb"`
+if [[ -n "$LIBRARY_UPDATED" ]]; then
+  echo "Your repository is not clean !"
+  exit 1
+fi
 
 echo "Ensuring Docker image $DOCKER_IMAGE_NAME exists ..."
 EXISTING_DOCKER_IMAGE=`docker images | grep "$DOCKER_IMAGE_NAME"`
@@ -19,20 +19,20 @@ fi
 echo "Updating library code to version $LIBRARY_NEW_VERSION ..."
 docker run --rm -v `pwd`:/gem/ "$DOCKER_IMAGE_NAME" rake update
 
-# LIBRARY_UPDATED=`git status --porcelain | grep -v make_new_release.sh`
-# if [[ -z "$LIBRARY_UPDATED" ]]; then
-#   echo "No update found, stopping release creation."
-#   exit 1
-# elif [[ "$LIBRARY_UPDATED" == " M lib/perfect-scrollbar-rails/version.rb" ]]; then
-#   echo "None of the JS or CSS files have been updated."
-#   exit 1
-# fi
-#
-# echo "Committing new version ..."
-# git commit -am "Import version $LIBRARY_NEW_VERSION"
-#
-# echo "Releasing gem ..."
-# docker run --rm -v ~/.gitconfig:/root/.gitconfig \
-#   -v ~/.ssh/:/root/.ssh/ \
-#   -v ~/.gem/:/root/.gem/ \
-#   -v `pwd`:/gem/ "$DOCKER_IMAGE_NAME" rake release
+LIBRARY_UPDATED=`git status --porcelain | grep -v make_new_release.sh`
+if [[ -z "$LIBRARY_UPDATED" ]]; then
+  echo "No update found, stopping release creation."
+  exit 1
+elif [[ "$LIBRARY_UPDATED" == " M lib/perfect-scrollbar-rails/version.rb" ]]; then
+  echo "None of the JS or CSS files have been updated."
+  exit 1
+fi
+
+echo "Committing new version ..."
+git commit -am "Import version $LIBRARY_NEW_VERSION"
+
+echo "Releasing gem ..."
+docker run --rm -v ~/.gitconfig:/root/.gitconfig \
+  -v ~/.ssh/:/root/.ssh/ \
+  -v ~/.gem/:/root/.gem/ \
+  -v `pwd`:/gem/ "$DOCKER_IMAGE_NAME" rake release
